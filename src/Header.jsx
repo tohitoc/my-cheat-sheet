5c474ab5f62a436c5fc94d2c2b771b36912155db
tohitoc <tohitob@gmail.com>
2025-10-12 19:10:34 +0900

    delete some of the boilerplate code

---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="">
+      <img src="./assets/header-logo.svg" alt="Header Logo" />
+      <button type="button">Sign in</button>
+    </header>
+  );
+}
