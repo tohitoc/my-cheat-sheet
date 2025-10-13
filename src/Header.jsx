d53ca231d1139bd8b7afd958424bdb0a67235eae
tohitoc <tohitob@gmail.com>
2025-10-13 18:08:31 +0900

    create header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,15 @@
+import headerLogo from './assets/header-logo.svg'
+import { Link } from "react-router";
+
 export default function Header() {
   return (
-    <header className="">
-      <img src="./assets/header-logo.svg" alt="Header Logo" />
-      <button type="button">Sign in</button>
+    <header className="bg-white flex justify-between items-center border-b border-black min-h-[61px] py-2 pl-8 pr-4 mb-15">
+      <Link>
+        <img src={headerLogo} alt="Header Logo" />
+      </Link>
+      <button className='bg-white font-inter font-2xl font-medium w-[102px] h-[35px]' type="button">
+        Sign in
+      </button>
     </header>
   );
 }
