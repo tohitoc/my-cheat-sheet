1a1b871caaa7ce874db477c8bb40e1c1b5354472
tohitoc <tohitob@gmail.com>
2025-10-23 01:44:56 +0900

    Add modal window to sheets page

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,8 @@
  * @module
  */
 
+import type * as sheet from "../sheet.js";
+
 import type {
   ApiFromModules,
   FilterApi,
@@ -22,7 +24,9 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  sheet: typeof sheet;
+}>;
 declare const fullApiWithMounts: typeof fullApi;
 
 export declare const api: FilterApi<
--- src/Sheets.jsx
+++ src/Sheets.jsx
@@ -1,19 +1,52 @@
 import Header from "./Header";
 import { Plus } from "lucide-react";
 import SheetList from "./SheetList";
+import { useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "../convex/_generated/api";
 
 export default function Sheets() {
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState("");
+  const addSheet = useMutation(api.sheet.addSheet);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await addSheet({ name: name });
+    setName("");
+  }
+  
   return (
     <>
       <Header />
       <div className="flex justify-between items-center w-full max-w-4/5 mx-auto mb-15">
         <h1 className="font-inter font-black text-5xl">Sheets</h1>
-        <button type="button" className="flex gap-3 px-2 py-1 rounded-lg justify-between items-center bg-e8e8e8 hover:bg-gray-300">
+        <button onClick={() => setOpen(true)} type="button" className="flex gap-3 px-2 py-1 rounded-lg justify-between items-center bg-e8e8e8 hover:bg-gray-300">
           <Plus size={24} />
           <span className="font-inter text-2xl font-medium">Add Sheet</span>
         </button>
       </div>
       <SheetList />
+      {open && (
+        <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center">
+          <form onSubmit={handleSubmit} className="bg-white py-5 px-6 w-[384px] min-h-[176px] rounded-sm">
+            <label className="font-inter text-xl font-normal block mb-2">Sheet Name</label>
+            <input className="border rounded-sm mb-9 w-full font-inter text-lg font-normal"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <div className="flex justify-between w-full">
+              <button onClick={() => setOpen(false)} type="button" className="hover:bg-gray-300 w-[162px] h-[44px] bg-e8e8e8 font-inter font-normal text-xl">
+                Cancel
+              </button>
+              <button type="submit" className="hover:bg-gray-300 w-[162px] h-[44px] bg-e8e8e8 font-inter font-normal text-xl">
+                OK
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </>
   );
 }
