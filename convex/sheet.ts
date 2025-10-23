7f5b8406d5466f4c147c1429d5916b6d7751fba9
tohitoc <tohitob@gmail.com>
2025-10-23 13:53:56 +0900

    Add functionality to fetch data from the database on the sheets page

--- convex/sheet.ts
+++ convex/sheet.ts
@@ -1,9 +1,16 @@
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 
 export const addSheet = mutation({
   args: { name: v.string() },
   handler: async (ctx, args) => {
     await ctx.db.insert("sheet", { name: args.name });
+  },
+});
+
+export const getSheetList = query({
+  handler: async (ctx) => {
+    const sheetList = await ctx.db.query("sheet").collect();
+    return sheetList;
   },
 });
--- src/SheetList.jsx
+++ src/SheetList.jsx
@@ -1,11 +1,19 @@
+import { api } from "../convex/_generated/api";
 import Sheet from "./Sheet";
+import { useQuery } from "convex/react";
 
 export default function SheetList() {
+  const sheetList = useQuery(api.sheet.getSheetList);
+
+  if (!sheetList) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="grid grid-cols-2 w-full max-w-3/4 mx-auto gap-x-20 gap-y-12 mb-15">
-      <Sheet>Figma</Sheet>
-      <Sheet>Linux</Sheet>
-      <Sheet>Vim</Sheet>
+      {sheetList.map((elem) => (
+        <Sheet key={elem._id}>{elem.name}</Sheet>
+      ))}
     </div>
   );
 }
