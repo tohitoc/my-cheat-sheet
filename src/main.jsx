c99514a105e84107da84faa09015da4db13b51b0
tohitoc <tohitob@gmail.com>
2025-10-29 16:52:54 +0900

    Install clerk

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router";
-import { ConvexProvider, ConvexReactClient } from 'convex/react';
+import { ConvexReactClient } from 'convex/react';
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ConvexProvider client={convex}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ConvexProvider>
+    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
   </StrictMode>,
 )
